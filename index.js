45f6e237579fa4e4a1f5df0bb59d0c6892ec7378
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-04 14:29:21 +0900

    web2-26 함수화(templateList)

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const url = require('url');
 //
 // });
 
+
+
 function templateHTML(title, list, description){
     return `
     <!doctype html>
@@ -36,35 +38,34 @@ function templateHTML(title, list, description){
     </html>
 `
 }
+function templateList(filelist){
+    let list = '<ul>';
+    for(let i=0; i<filelist.length; i++){
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    }
+    list += '</ul>'
+    return list;
+}
 const app = http.createServer(function (request, response) {
     const _url = request.url
     const queryData = url.parse(_url, true).query
     const pathname = url.parse(_url, true).pathname
+
     if (pathname === '/') {
         if (queryData.id === undefined) {
             const title = 'Welcome'
             const description = 'Hello, Node.js'
             fs.readdir('data/',function(err,data){
-                let list = '<ul>';
-                for(let i=0; i<data.length; i++){
-                    list += `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
-                }
-                list += '</ul>'
-
+                let list = templateList(data);
                 const template = templateHTML(title,list,description);
-
                 response.writeHead(200);
                 response.end(template);
             })
         } else {
             fs.readdir('data/', function(err,data){
-                let list = '<ul>';
-                for(let i=0; i<data.length; i++){
-                    list += `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
-                }
-                list += '</ul>';
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     const title = queryData.id;
+                    let list = templateList(data);
                     const template = templateHTML(title, list, description);
                     response.writeHead(200);
                     response.end(template);
