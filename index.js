3298cca8131b4bbbc0f753d4873c8ab88aa26b7c
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-04 13:49:34 +0900

    web2-26 함수화(templateHTML)

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ const url = require('url');
 //
 //
 // });
+
+function templateHTML(title, list, description){
+    return `
+    <!doctype html>
+    <html log="ko">
+    <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>                
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+    </body>
+    </html>
+`
+}
 const app = http.createServer(function (request, response) {
     const _url = request.url
     const queryData = url.parse(_url, true).query
@@ -32,24 +50,11 @@ const app = http.createServer(function (request, response) {
                     list += `<li><a href="/?id=${data[i]}">${data[i]}</a></li>`;
                 }
                 list += '</ul>'
-                const template = `
-                    <!doctype html>
-                    <html lang="ko">
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        
-                        ${list}
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                    </body>
-                    </html>
-                    `
-                response.writeHead(200)
-                response.end(template)
+
+                const template = templateHTML(title,list,description);
+
+                response.writeHead(200);
+                response.end(template);
             })
         } else {
             fs.readdir('data/', function(err,data){
@@ -59,33 +64,18 @@ const app = http.createServer(function (request, response) {
                 }
                 list += '</ul>';
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                    const title = queryData.id
-                    const template = `
-                    <!doctype html>
-                    <html lang="ko">
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        
-                        ${list}
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                    </body>
-                    </html>
-                    `
-                    response.writeHead(200)
-                    response.end(template)
+                    const title = queryData.id;
+                    const template = templateHTML(title, list, description);
+                    response.writeHead(200);
+                    response.end(template);
                 })
             });
         }
     } else {
-        response.writeHead(404)
-        response.end('Not found')
+        response.writeHead(404);
+        response.end('Not found');
     }
-})
+});
 
 app.listen(3333);
 
