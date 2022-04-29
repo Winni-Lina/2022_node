f01f9b7a0b150ec537eafc11bfc5c28f4341b946
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-29 14:55:08 +0900

    web2-47 출력정보에 대한 보안

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
+const sanitizeHtml = require('sanitize-html');
 
 const template = {
     List: function templateList(fileList) {
@@ -57,10 +58,15 @@ const app = http.createServer(function (request, response) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     const title = queryData.id
                     const list = template.List(data);
+
+                    //xss방지 (게시글 제목과 내용에 script를 못넣도록 함)
+                    const sanitizedTitle = sanitizeHtml(title);
+                    const sanitizedDescription = sanitizeHtml(description)
+
                     // 특정 게시글을 읽고 있을 땐 create(새 게시글 생성)와 update(게시글 수정)을 보이게
-                    const html = template.HTML(title, list, description, `<a href="/create">create</a>&nbsp;<a href="/update?id=${title}">update</a>
+                    const html = template.HTML(sanitizedTitle, list, sanitizedDescription, `<a href="/create">create</a>&nbsp;<a href="/update?id=${title}">update</a>
                         <form action="delete_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
+                        <input type="hidden" name="id" value="${sanitizedTitle}">
                         <input type="submit" value="delete">
                         </form>`);
                     response.writeHead(200)
