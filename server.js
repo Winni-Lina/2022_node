92f7cb198f4cfedc6253bee25b78dd0d204f2bbe
vinszip999 <w2011@e-mirim.hs.kr>
2022-06-13 14:23:19 +0900

    DB 연결

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
+mongoose.connect(process.env.DB_CONNECTION
+, {useNewUrlParser: true, useUnifiedTopology: true}
+);
+const db = mongoose.connection;
+db.on('error', function (err){
+    console.log(err)
+})
+
+db.once('open',function (){
+    console.log('디비연결')
+})
 
 app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT);
+
