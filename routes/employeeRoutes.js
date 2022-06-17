c47183278745c591f4ee21eae1369ddb9482eb4d
vinszip999 <w2011@e-mirim.hs.kr>
2022-06-17 14:40:20 +0900

    UPDATE, DELETE 구현

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const EmployeeController = require('../controllers/employeeController');
+const {model} = require("mongoose");
+
+router.get('/showAll', EmployeeController.showAll);
+router.get('/show', EmployeeController.show);
+router.post('/insert', EmployeeController.insert);
+router.put('/update', EmployeeController.update);
+router.delete('/delete', EmployeeController.destory);
+
+module.exports = router;
+
